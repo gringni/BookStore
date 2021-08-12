cc1fcb575632ab0dd48bfb503bac120a22ce9c77
gringni <gringni@gmail.com>
2021-08-12 17:22:38 +0700

    create function Name

--- src/main/java/bookStore/demo/bookStoreController.java
+++ src/main/java/bookStore/demo/bookStoreController.java
@@ -1,7 +1,9 @@
 package bookStore.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +19,19 @@ public class bookStoreController {
         this.bookStoreService = bookStoreService;
     }
 
-    @GetMapping
+    @GetMapping (value = "/user")
     public List<user> getUser(){
         return bookStoreService.getUser();
     }
+
+    @DeleteMapping (value = "/user")
+    public void deleteUser(){return bookStoreService.deleteUser();}
+
+    @PostMapping (value = "/user")
+    public void postUser(){return bookStoreService.postUser();}
+
+    @PostMapping (value = "/user/orders")
+    public void postOrder(){return bookStoreService.postOrder();}
+
+    
 }
